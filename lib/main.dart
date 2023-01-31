e5dc52d8b57acfaf89aa5110905428fc90655d43
Fuat Hüriyetoğlu <fuat.huriyetoglu@gmail.com>
2023-01-31 21:25:56 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+
+
+
+void main() {
+  print('a'.isNotEmpty);
+  int i = 1;
+  print(1.isEven);
+  print(i);
+  String maintekcal;
+  maintekcal = 'main ' + 'tekrar ' + 'çalıştı';
+
+  print(maintekcal);
+  int index = maintekcal.indexOf("ar");
+  print(index);
+
+  print('main bitmek üzere');
+
+
+
+
+}
